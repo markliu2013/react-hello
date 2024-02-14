7bf26da63a60d4cfc65810cb7f6df61d557a39fa
anonymous <anonymous@1.com>
2024-02-14 16:56:19 +0800

    chore: lint on commitmsg

--- webpack.config.js
+++ webpack.config.js
@@ -26,15 +26,6 @@ module.exports = {
         use: {
           loader: 'ts-loader'
         }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: 'url-loader',
-        options: { limit: false }
       }
     ]
   }
